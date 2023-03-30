5e8fe3c2c5f238284dca1f2d641706e7428cd497
VictorHugo110199 <victorhugo110199@gmail.com>
2023-03-30 00:08:09 -0300

    fix: User

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+  nameUser: "Victor Hugo Santos",
+  githubUser: "VictorHugo110199",
+  linkedinUser: "victor-hugo-santos-silva-b5ab7a144",
+  whatsappNumber: "+5518998024893",
+  emailUser: "victorhugo110199@gmail.com",
+  instagramUser: "victorsilva9412",
+  facebookUser: "Victor Silva",
 };
