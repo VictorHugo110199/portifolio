4e131b32f1341f980b7910ecf92b35a45f77870a
VictorHugo110199 <victorhugo110199@gmail.com>
2023-05-03 18:41:45 -0300

    fix: main

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,5 +1,11 @@
 import { TbBrandNextjs } from "react-icons/tb";
-import { SiPostgresql, SiTypescript } from "react-icons/si";
+import {
+  SiPostgresql,
+  SiTypescript,
+  SiSqlite,
+  SiDjango,
+  SiStyledcomponents,
+} from "react-icons/si";
 import { FaGit, FaPython } from "react-icons/fa";
 import {
   FaDocker,
@@ -32,9 +38,10 @@ export const stackData = [
     title: "React",
     img: FaReact,
   },
-  { title: "Next.js", img: TbBrandNextjs },
-  { title: "Docker", img: FaDocker },
   { title: "PostgreSQL", img: SiPostgresql },
   { title: "Git", img: FaGit },
   { title: "Python", img: FaPython },
+  { title: "SQLite", img: SiSqlite },
+  { title: "DJango", img: SiDjango },
+  { title: "StyledComponents", img: SiStyledcomponents },
 ];
